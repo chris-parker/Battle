fad5b1ef3a39367527cbdff6860426db6b2332e0
Chris Parker <cjp214@gmail.com>
2016-03-16 12:18:12 +0000

    made the controller skinnier

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,13 @@
 class Game
-  # 
-  # def initialize(player = Player.new)
-  #   @player = player
-  # end
 
-  def attack(player)
-    player.receive_damage
+  attr_reader :player_1, :player_2
+
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def attack(player_2)
+    @player_2.receive_damage
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,17 @@
 require 'game'
 
 describe Game  do
-  subject(:game) { described_class.new player }
-  let(:player) { double :player }
-  it 'reduces hit points' do
-    expect(player).to receive(:receive_damage)
-    subject.attack(player)
+  subject(:game) { described_class.new player_1, player_2 }
+  let(:player_1) { double :player_1 }
+  let(:player_2) { double :player_2 }
+
+  it 'starts a game with two players' do
+    expect(subject.player_1).to be_truthy
   end
+
+  it 'reduces hit points' do
+    expect(player_2).to receive(:receive_damage)
+    subject.attack(player_2)
+  end
+
 end
