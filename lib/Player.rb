320e77fd7c3fedac71e99cfb6f0924b6467b0f7a
Chris Parker <cjp214@gmail.com>
2016-03-16 11:13:14 +0000

    added hit_points method

--- lib/Player.rb
+++ lib/Player.rb
@@ -1,6 +1,12 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :hit_points
   def initialize name
     @name = name
+    @hit_points = 60
   end
+
+  def attacked
+    @hit_points -= 10
+  end
+
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -6,3 +6,11 @@ feature 'Checking hit points' do
     expect(page).to have_content('Murilo has 60/60 HP')
   end
 end
+
+feature 'Reducing hit points' do
+  scenario 'Returns player 2s reduced hit points' do
+    sign_in_and_play
+    click_button "Attack!"
+    expect(page).to have_content('Murilo has 50/60 HP')
+  end
+end
